6359aa522e056d67ff4d154e32b48e720c7c840a
jw755100@gmail.com <jw755100@gmail.com>
2023-03-16 16:57:02 +0900

    five

--- 2023.03.16.py
+++ 2023.03.16.py
@@ -18,10 +18,13 @@ b = '하세요'
 c = a + b
 d = 1
 print(c, d) #콤마(,) 쓰면 한칸이 띄워진다. 붙이려면 문자열을 더하기 연산자로 써야됨
-print(c, end="") #end="" 쓰면 붙여진다
+print(c, end="") #end="" or end='' 쓰면 붙여진다
 print(d)
 print(a + b, '\n반갑습니다') #\n쓰면 줄 띄워짐
 print(a + b, end="")
 print('반갑습니다')
 '''
 
+print("결과값은", 2 * 7, "입니다.") #문자열과 숫자는 더하기 연산자 안된다.
+print("결과값은", 2 * 7, end='')
+print('입니다')
