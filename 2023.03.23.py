110467dc3a693ba090e08ba27e3da49805dcab37
jw755100@gmail.com <jw755100@gmail.com>
2023-03-23 17:00:15 +0900

    second

--- 2023.03.23.py
+++ 2023.03.23.py
@@ -1,3 +1,4 @@
+'''
 #대화하는 프로그램 만들기
 price = int(input('물건값을 입력하시오: '))
 thousands= int(input('1000원 지폐개수: '))
@@ -15,3 +16,36 @@ change10 = (change % 50) // 10
 change1 = (change % 10) // 1
 
 print('500원=', change500, ', 100원=', change100, ', 50원=', change50, ', 10원=', change10, ', 1원=', change1)
+'''
+
+'''
+a = 'Hello.exe'
+print(a, a[0], a[0:4], a[-1])
+print(a[2:])
+print(a[-3:])   #확장자만 뽑을 때
+
+a = '"hello.exe"'
+print(a)
+b = "'hello.exe'"
+print(b)
+'''
+
+'''
+line = "=" * 50
+print(line)
+'''
+
+'''
+a = input('첫 번째 단어를 입력해주세요: ')
+b = input('두 번째 단어를 입력해주세요: ')
+c = input('세 번째 단어를 입력해주세요: ')
+a.upper()
+b.upper()
+c.upper()
+print(a[0] + b[0] + c[0])
+'''
+
+a = input('첫 번째 단어를 입력해주세요: ')
+b = input('두 번째 단어를 입력해주세요: ')
+c = input('세 번째 단어를 입력해주세요: ')
+print(a[0].lower() + b[0].lower() + c[0].lower())
