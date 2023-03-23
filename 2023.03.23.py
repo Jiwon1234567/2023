5e157f9c633559b0be7b46615f2f4802e448719c
jw755100@gmail.com <jw755100@gmail.com>
2023-03-23 17:53:40 +0900

    third

--- 2023.03.23.py
+++ 2023.03.23.py
@@ -45,7 +45,80 @@ c.upper()
 print(a[0] + b[0] + c[0])
 '''
 
+'''
 a = input('첫 번째 단어를 입력해주세요: ')
 b = input('두 번째 단어를 입력해주세요: ')
 c = input('세 번째 단어를 입력해주세요: ')
 print(a[0].lower() + b[0].lower() + c[0].lower())
+'''
+
+'''
+a, b, c = input("정수 두개: ").split() #split는 문자열로만 받을 수 있다
+print(int(a) + int(b) + int(c))  #int로 하나씩 일일이 바꿔줘야됨
+'''
+
+'''
+a, b, c = input('단어 세개를 입력해주세요: ').split()
+print(a[0].upper() + b[0].upper() + c[0].upper())
+'''
+
+'''
+age, name = input('나이와 이름을 입력해주세요 ').split()
+print(name +'님 10년 후 나이는', int(age) + 10,'입니다')
+'''
+
+'''
+age, name = input('나이와 이름을 입력해주세요 ').split()
+print(name +'님 10년 후 나이는', int(age) + 10, end="")
+print('입니다')
+'''
+
+'''
+score = int(input('점수를 입력하세요: '))
+if score >= 60:
+    print('합격입니다.')
+else:
+    print('불합격입니다.')
+'''
+
+#3장 선택
+
+'''
+food = '스테이크'
+if food == '스테이크':
+    print('내가 제일 좋아하는 음식!')
+    print(10 * food)
+'''
+
+'''
+food = ['스테이크', '불고기', '햄버거', '치킨']
+you = input('좋아하는 음식은 무엇입니까? ')
+
+if you in food:
+    print('취향이 같군요')
+else:
+    print('취향이 다르군요
+'''
+
+'''
+weight = float(input('짐의 무게는 얼마입니까? '))
+if weight < 21:
+    print('짐에 대한 수수료는 없습니다.')
+else:
+    print('무거운 짐은 20,000원을 내셔야 합니다.')
+print('감사합니다.')
+'''
+
+'''
+num = int(input('정수를 입력하시오: '))
+if num % 2 == 0:
+    print('입력된 정수는 짝수입니다.')
+else:
+    print('입력된 정수는 홀수입니다.')
+'''
+
+n1, n2 = input('두 개의 정수를 입력해주세요: ').split()
+if int(n1) > int(n2):
+    print('큰 수는', int(n1))
+else:
+    print('큰 수는', int(n2))
