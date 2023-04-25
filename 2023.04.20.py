fd6f9e138d0f60f5269e4e0993f09b7613693ad7
jw755100@gmail.com <jw755100@gmail.com>
2023-04-25 23:14:03 +0900

    오류수정

--- 2023.04.20.py
+++ 2023.04.20.py
@@ -28,7 +28,7 @@ get_sum(1, 10)
 get_sum(1, 20)
 '''
 
-
+'''
 def get_sum(x, y):
     sum = x + y
     return sum
@@ -38,4 +38,39 @@ n1, n2 = input('숫자 두개를 넣으시오: ').split()
 minNum = min(int(n1), int(n2))
 maxNum = max(int(n1), int(n2))
 get_sum(minNum, maxNum)
+'''
 
+
+'''
+def main():
+    n1, n2 = input('숫자 두개를 넣으시오: ').split()
+    minNum = min(int(n1), int(n2))
+    maxNum = max(int(n1), int(n2))
+    get_sum(minNum, maxNum)
+    
+def get_sum(x, y):
+    sum = x + y
+    return sum
+
+main()      #이 형태를 가장 많이 씀
+'''
+
+'''
+def say_hello(name, msg):
+    print('안녕', name, msg)
+
+#say_hello('철수', '어서와')
+#say_hello('길똥', '집에와')
+name = input('이름을 넣어주세요: ')
+msg = input('메세지를 넣어주세요: ')
+say_hello(name, msg)
+'''
+
+
+def say_hello(name, msg):
+    print('안녕', name, msg)
+
+for i in range(3):
+    name = input('이름을 넣어주세요: ')
+    msg = input('메세지를 넣어주세요: ')
+    say_hello(name, msg)
--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-for i in range(5):
-    print('*'* (10-i))
+num = int(input('숫자를 넣으세요: '))
+sum = 0
+while num > 0:            #조건 중요???? num > 0
+    sum += num % 10
+    num //= 10
+print('자리수의 합은', sum, '입니다')
+print('자리수의 합은 %d입니다' % sum)
