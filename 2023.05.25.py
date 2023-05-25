40baba38c82896fb5e0114ad7f212e3cee7312fe
jw755100@gmail.com <jw755100@gmail.com>
2023-05-25 16:15:03 +0900

    second

---
+++ 2023.05.25.py
@@ -0,0 +1,20 @@
+#6장
+
+'''
+#p.2
+a = [10, 20, 30]
+a[0] = 40
+#a[3] = 50 -> 안됨
+a.append(50)
+print(a)
+'''
+
+
+scores = []
+for i in range(5):
+    a = int(input('성적을 입력하시오: '))
+    scores.append(a)
+print(scores)
+
+
+
