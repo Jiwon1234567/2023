28a0591b7e4cb1893cab21007f44b1830418d94a
jw755100@gmail.com <jw755100@gmail.com>
2023-06-14 20:38:17 +0900

    6/14 second

--- test2.py
+++ test2.py
@@ -1,11 +1,30 @@
-from math import sqrt
+def print_menu():
+    print('-'*20)
+    print('1. 친구 리스트 출력')
+    print('2. 친구추가')
+    print('3. 친구삭제')
+    print('4. 이름변경')
+    print('9. 종료')
 
-def distance(a1, b1, a2, b2):
-    distance = sqrt((int(a2) - int(a1)) ** 2 + (int(b2) - int(b1)) ** 2)
-    return distance
-
-x1, y1 = input('한점을 입력하세요: ').split()
-x2, y2 = input('다른 한점을 입력하세요: ').split()
-
-d = distance(x1, y1, x2, y2)
-print('('+ x1 + ',' + y1 + ')과 (' + x2 + ',' + y2 + ')의 길이는 %f입니다.' %d)     #결과가 소수로 나올 수 있으므로 %f
+friend = []
+#menu = 5
+while True:
+    print_menu()
+    menu = int(input('메뉴를 선택하시오: '))
+    if menu == 1:
+        print(friend)
+    elif menu == 2:
+        friend_name = input('이름을 입력하시오: ')
+        friend.append(friend_name)
+    elif menu == 3:
+        friend_name = input('이름을 입력하시오: ')
+        friend.remove(friend_name)
+    elif menu == 4:
+        friend_name = input('이름을 입력하시오: ')
+        replace = friend.index(friend_name)
+        new_name = input('새로운 이름을 입력하시오: ')
+        friend[replace] = new_name
+    elif menu == 9:
+        break
+    else:
+        print('잘못 입력하셨습니다.')
